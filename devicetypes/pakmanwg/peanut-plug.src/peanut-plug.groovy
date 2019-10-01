2095a8abca6880cd172c6893a46ba96491c5ea68
pakmanwg <pakmanw@sbcglobal.net>
2019-09-30 23:57:26 -0700

    Update peanut-plug.groovy

--- devicetypes/pakmanwg/peanut-plug.src/peanut-plug.groovy
+++ devicetypes/pakmanwg/peanut-plug.src/peanut-plug.groovy
@@ -23,7 +23,7 @@
  *                      add support for new smartthings app
  *  2019-01-17 - v01.04 merging jamesham retain state code
  *  2019-09-27 - v01.05 update fingerprint from transman
- *  2019-09-28 - v01.06 add interval, and cost settings
+ *  2019-09-30 - v01.06 new interface, add active power settings
  */
 
 import physicalgraph.zigbee.zcl.DataType
@@ -53,109 +53,72 @@ metadata {
 	}
 
 	// tile definitions
-	tiles {
-		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-			state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC"
-			state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+	tiles(scale: 2) {
+		multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4, canChangeIcon: true) {
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc"
+				attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			}
+			tileAttribute ("device.acceleration", key: "SECONDARY_CONTROL") {
+				attributeState "inactive", label:'INACTIVE'
+				attributeState "active", label:'ACTIVE'
+			}
 		}
-		valueTile("power", "device.power") {
-			state "default", label:'${currentValue} W'
+		standardTile("refresh", "device.refresh", width: 2, height: 2) {
+			state "refresh", label:'Refresh', action: "refresh", icon:"st.secondary.refresh-icon"
 		}
-		valueTile("energy", "device.energy") {
-			state "default", label:'${currentValue} kWh'
+		standardTile("reset", "device.reset", width: 2, height: 2) {
+			state "refresh", label:'Reset', action: "reset", icon:"st.secondary.refresh-icon"
 		}
-		valueTile("voltage", "device.voltage") {
-			state "default", label:'${currentValue} V'
+		valueTile("power", "device.power", width: 2, height: 2) {
+			state "default", label:'${currentValue} W', backgroundColor: "#cccccc"
 		}
-		valueTile("current", "device.current") {
-			state "default", label:'${currentValue} A'
+		valueTile("energy", "device.energy", width: 2, height: 2) {
+			state "default", label:'${currentValue} kWh', backgroundColor: "#cccccc"
 		}
-		valueTile("cost", "device.cost") {
-			state "default", label:'$ ${currentValue}'
+		valueTile("voltage", "device.voltage", width: 2, height: 2) {
+			state "default", label:'${currentValue} V', backgroundColor: "#cccccc"
 		}
-		standardTile("reset", "device.energy", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'reset kWh', action:"reset"
+		valueTile("current", "device.current", width: 2, height: 2) {
+			state "default", label:'${currentValue} A', backgroundColor: "#cccccc"
 		}
-		standardTile("refresh", "device.power", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+		valueTile("history", "device.history", decoration:"flat", width: 6, height: 3) {
+			state "history", label:'${currentValue}'
 		}
 
-		main(["switch","power","energy","voltage","current","cost"])
-		details(["switch","power","energy","voltage","current","cost","refresh","reset"])
+		main(["switch"])
+		details(["switch", "power", "energy", "refresh", "voltage", "current", "reset", "history"])
 	}
 
 	preferences {
 		input name: "retainState", type: "bool", title: "Retain State?", description: "Retain state on power loss?", required: false, displayDuringSetup: false, defaultValue: true
-		// input "reportPercentageChanges", "number", title: "Report Percentage Changes", description: "Report Percentage Changes", range: "0..*", defaultValue: 5
-		input "reportIntervalMinutes", "number", title: "Report Interval in minute for voltage, current", description: "Time between Report in minute", range: "0..*", defaultValue: 5
-		input "costPerKwh", "number", title: "Cost per Kwh in cent", description: "Electric Cost per Kwh in cent", range: "0..*", defaultValue: 15       
+		// configParams?.each {			
+		// 	getOptionsInput(it)
+		// }
+		// input "energyPrice", "number", title: "\$/kWh Cost:", description: "Electric Cost per Kwh in cent", range: "0..*", defaultValue: 15
+		input "inactivePower", "decimal", title: "Reporting inactive when power is less than or equal to:", range: "0..*", defaultValue: 0
+		// ["Power", "Energy", "Voltage", "Current"].each {
+		// 	getBoolInput("display${it}", "Display ${it} Activity", true)
+		// }
 	}
 }
 
-def parse(String description) {
-
-	log.debug "description is $description"
-	def event = zigbee.getEvent(description)
-	if (event) {
-		if (event.name == "power") {
-			def powerValue
-			powerValue = (event.value as Integer) * getPowerMultiplier()
-			sendEvent(name: "power", value: powerValue)
-			costValue = powerValue * costPerKwh
-			sendEvent(name: "cost", value: costValue)
-			def time = (now() - state.time) / 3600000 / 1000
-			state.time = now()
-			// log.debug "powerValues is $state.powerValue"
-			state.energyValue = state.energyValue + (time * state.powerValue)
-			state.powerValue = powerValue
-			// log.debug "energyValue is $state.energyValue"
-			sendEvent(name: "energy", value: state.energyValue)
-			state.costValue = state.energyValue * costPerKwh
-			sendEvent(name: "cost", value: state.costValue)
-		} else {
-			sendEvent(event)
-		}
-	} else if (description?.startsWith("read attr -")) {
-		def descMap = zigbee.parseDescriptionAsMap(description)
-		log.debug "Desc Map: $descMap"
-		if (descMap.clusterInt == zigbee.ELECTRICAL_MEASUREMENT_CLUSTER) {
-			def intVal = Integer.parseInt(descMap.value,16)
-			if (descMap.attrInt == 0x0600) {
-				// log.debug "ACVoltageMultiplier $intVal"
-				state.voltageMultiplier = intVal
-			} else if (descMap.attrInt == 0x0601) {
-				// log.debug "ACVoltageDivisor $intVal"
-				state.voltageDivisor = intVal
-			} else if (descMap.attrInt == 0x0602) {
-				// log.debug "ACCurrentMultiplier $intVal"
-				state.currentMultiplier = intVal
-			} else if (descMap.attrInt == 0x0603) {
-				// log.debug "ACCurrentDivisor $intVal"
-				state.currentDivisor = intVal
-			} else if (descMap.attrInt == 0x0604) {
-				// log.debug "ACPowerMultiplier $intVal"
-				state.powerMultiplier = intVal
-			} else if (descMap.attrInt == 0x0605) {
-				// log.debug "ACPowerDivisor $intVal"
-				state.powerDivisor = intVal
-			} else if (descMap.attrInt == 0x0505) {
-				def voltageValue = intVal * getVoltageMultiplier()
-				// log.debug "Voltage ${voltageValue}"
-				state.voltage = $voltageValue
-				sendEvent(name: "voltage", value: voltageValue)
-			} else if (descMap.attrInt == 0x0508) {
-				def currentValue = intVal * getCurrentMultiplier()
-				// log.debug "Current ${currentValue}"
-				state.current = $currentValue
-				sendEvent(name: "current", value: currentValue)
-			}
-		} else {
-			log.warn "Not an electrical measurement"
-		}
-	} else {
-		log.warn "DID NOT PARSE MESSAGE for description : $description"
-		log.debug zigbee.parseDescriptionAsMap(description)
+private getOptionsInput(param) {
+	if (param.prefName) {
+		input "${param.prefName}", "enum",
+			title: "${param.name}:",
+			defaultValue: "${param.val}",
+			required: false,
+			displayDuringSetup: true,
+			options: param.options?.collect { name, val -> name }
 	}
+}
+
+private getBoolInput(name, title, defaultVal) {
+	input "${name}", "bool", 
+		title: "${title}?", 
+		defaultValue: defaultVal, 
+		required: false
 }
 
 def installed() {
@@ -173,7 +136,7 @@ def on() {
 }
 
 def refresh() {
-	// Integer reportIntervalMinutes = 5
+	Integer reportIntervalMinutes = 5
 	setRetainState() +
 	zigbee.onOffRefresh() +
 	zigbee.simpleMeteringPowerRefresh() +
@@ -276,4 +239,320 @@ def reset() {
 	state.time = now()
 	sendEvent(name: "energy", value: state.energyValue)
 	sendEvent(name: "cost", value: state.costValue)
+}
+
+def refreshHistory() {
+	def history = ""
+	def items = [:]
+			
+	items["energyDuration"] = "Energy - Duration"
+	items["energyCost"] = "Energy - Cost"
+	["power", "voltage", "current"].each {
+		items["${it}Low"] = "${it.capitalize()} - Low"
+		items["${it}High"] = "${it.capitalize()} - High"
+	}
+	items.each { attrName, caption ->
+		def attr = device.currentState("${attrName}")
+		def val = attr?.value ?: ""
+		def unit = attr?.unit ?: ""
+		history += "${caption}: ${val} ${unit}\n"
+	}
+	sendEvent(createEventMap("history", history, false))
+}
+
+// Configuration Parameters
+private getConfigParams() {
+	return [
+		powerValueChangeParam,
+		powerPercentageChangeParam,
+		powerReportIntervalParam,
+		energyReportIntervalParam,
+		voltageReportIntervalParam,
+		electricityReportIntervalParam
+	]
+}
+
+private getPowerValueChangeParam() {
+	return createConfigParamMap(151, "Power Report Value Change", 2, getPowerValueOptions(), "powerValueChange")
+}
+
+private getPowerPercentageChangeParam() {
+	return createConfigParamMap(152, "Power Report Percentage Change", 1, getPercentageOptions(10, "No Reports"), "powerPercentageChange")
+}
+
+private getPowerReportIntervalParam() {
+	return createConfigParamMap(171, "Power Reporting Interval", 4, getIntervalOptions(30, "No Reports"), "powerReportingInterval")
+}
+
+private getEnergyReportIntervalParam() {
+	return createConfigParamMap(172, "Energy Reporting Interval", 4, getIntervalOptions(300, "No Reports"), "energyReportingInterval")	
+}
+
+private getVoltageReportIntervalParam() {
+	return createConfigParamMap(173, "Voltage Reporting Interval", 4, getIntervalOptions(0, "No Reports"), "voltageReportingInterval")	
+}
+
+private getElectricityReportIntervalParam() {
+	return createConfigParamMap(174, "Electrical Current Reporting Interval", 4, getIntervalOptions(0, "No Reports"), "electricityReportingInterval")	
+}
+
+private createConfigParamMap(num, name, size, options, prefName, val=null) {
+	if (val == null) {
+		val = (settings?."${prefName}" ?: findDefaultOptionName(options))
+	}
+	return [
+		num: num, 
+		name: name, 
+		size: size, 
+		options: options, 
+		prefName: prefName,
+		val: val
+	]
+}
+
+// Settings
+private getEnergyPriceSetting() {
+	return safeToDec(settings?.energyPrice, 0.12)
+}
+
+private getInactivePowerSetting() {
+	return safeToDec(settings?.inactivePower, 0)
+}
+
+private getDebugOutputSetting() {
+	return settings?.debugOutput != false
+}
+
+private getMinimumReportingInterval() {
+	def minVal = (60 * 60 * 24 * 7)
+	[powerReportIntervalParam, energyReportIntervalParam, voltageReportIntervalParam, electricityReportIntervalParam].each {
+		def val = convertOptionSettingToInt(it.options, it.val)
+		if (val && val < minVal) {
+			minVal = val
+		}		
+	}
+	return minVal
+}
+
+private getIntervalOptions(defaultVal=null, zeroName=null) {
+	def options = [:]
+	if (zeroName) {
+		options["${zeroName}"] = 0
+	}
+	options << getIntervalOptionsRange("Second", 1, [5,10,15,30,45])
+	options << getIntervalOptionsRange("Minute", 60, [1,2,3,4,5,10,15,30,45])
+	options << getIntervalOptionsRange("Hour", (60 * 60), [1,2,3,6,9,12,18])
+	options << getIntervalOptionsRange("Day", (60 * 60 * 24), [1,3,5])
+	options << getIntervalOptionsRange("Week", (60 * 60 * 24 * 7), [1,2])
+	return setDefaultOption(options, defaultVal)
+}
+
+private getIntervalOptionsRange(name, multiplier, range) {
+	def options = [:]
+	range?.each {
+		options["${it} ${name}${it == 1 ? '' : 's'}"] = (it * multiplier)
+	}
+	return options
+}
+
+private getPowerValueOptions() {
+	def options = [:]	
+	[0,1,2,3,4,5,10,25,50,75,100,150,200,250,300,400,500,750,1000,1250,1500,1750,2000,2500,3000,3500,4000,4500,5000,6000,7000,8000,9000,10000,12500,15000].each {		
+		if (it == 0) {
+			options["No Reports"] = it
+		}
+		else {
+			options["${it} Watts"] = it
+		}
+	}
+	return setDefaultOption(options, 50)
+}
+
+private getPercentageOptions(defaultVal=null, zeroName=null) {
+	def options = [:]
+	if (zeroName) {
+		options["${zeroName}"] = 0
+	}	
+	for (int i = 1; i <= 5; i += 1) {
+		options["${i}%"] = i
+	}		
+	for (int i = 10; i <= 100; i += 5) {
+		options["${i}%"] = i
+	}	
+	return setDefaultOption(options, defaultVal)
+}
+
+private convertOptionSettingToInt(options, settingVal) {
+	return safeToInt(options?.find { name, val -> "${settingVal}" == name }?.value, 0)
+}
+
+private setDefaultOption(options, defaultVal) {
+	def name = options.find { key, val -> val == defaultVal }?.key
+	if (name != null) {
+		return changeOptionName(options, defaultVal, "${name}${defaultOptionSuffix}")
+	}
+	else {
+		return options
+	}	
+}
+
+private changeOptionName(options, optionVal, newName) {
+	def result = [:]	
+	options?.each { name, val ->
+		if (val == optionVal) {
+			name = "${newName}"
+		}
+		result["${name}"] = val
+	}
+	return result
+}
+
+private findDefaultOptionName(options) {
+	def option = options?.find { name, val ->
+		name?.contains("${defaultOptionSuffix}") 
+	}
+	return option?.key ?: ""
+}
+
+private getDefaultOptionSuffix() {
+	return "   (Default)"
+}
+
+private createEventMap(name, value, displayed=null, desc=null, unit=null) {	
+	desc = desc ?: "${name} is ${value}"
+	
+	def eventMap = [
+		name: name,
+		value: value,
+		displayed: (displayed == null ? ("${getAttrVal(name)}" != "${value}") : displayed)
+	]
+	
+	if (unit) {
+		eventMap.unit = unit
+		desc = "${desc} ${unit}"
+	}
+	
+	if (desc && eventMap.displayed) {
+		logDebug desc
+		eventMap.descriptionText = "${device.displayName} - ${desc}"
+	}
+	else {
+		logTrace "Creating Event: ${eventMap}"
+	}
+	return eventMap
+}
+
+private getAttrVal(attrName) {
+	try {
+		return device?.currentValue("${attrName}")
+	}
+	catch (ex) {
+		logTrace "$ex"
+		return null
+	}
+}
+
+private safeToInt(val, defaultVal=0) {
+	return "${val}"?.isInteger() ? "${val}".toInteger() : defaultVal
+}
+
+private safeToDec(val, defaultVal=0) {
+	return "${val}"?.isBigDecimal() ? "${val}".toBigDecimal() : defaultVal
+}
+
+private roundTwoPlaces(val) {
+	return Math.round(safeToDec(val) * 100) / 100
+}
+
+private convertToLocalTimeString(dt) {
+	def timeZoneId = location?.timeZone?.ID
+	if (timeZoneId) {
+		return dt.format("MM/dd/yyyy hh:mm:ss a", TimeZone.getTimeZone(timeZoneId))
+	}
+	else {
+		return "$dt"
+	}	
+}
+
+private isDuplicateCommand(lastExecuted, allowedMil) {
+	!lastExecuted ? false : (lastExecuted + allowedMil > new Date().time) 
+}
+
+def parse(String description) {
+
+	// log.debug "description is $description"
+	def event = zigbee.getEvent(description)
+	if (event) {
+		if (event.name == "power") {
+			def powerValue
+			powerValue = (event.value as Integer) * getPowerMultiplier()
+			sendEvent(name: "power", value: powerValue)
+			def time = (now() - state.time) / 3600000 / 1000
+			state.time = now()
+			// log.debug "powerValues is $state.powerValue"
+			state.energyValue = state.energyValue + (time * state.powerValue)
+			state.powerValue = powerValue
+			// log.debug "energyValue is $state.energyValue"
+			def localCostPerKwh = 15
+			if (energyPrice) {
+				localCostPerKwh = energyPrice as Integer
+			}  
+			sendEvent(name: "energy", value: state.energyValue)
+			state.costValue = state.energyValue * localCostPerKwh / 100
+			sendEvent(name: "cost", value: state.costValue)
+			def deviceActive = (device.currentValue("acceleration") == "active")
+			if (inactivePowerSetting == null) {
+				inactivePowerSetting = 0
+			}
+			if (powerValue > inactivePowerSetting && !deviceActive) {
+				sendEvent(name:"acceleration", value:"active", displayed:false)
+			}
+			else if (powerValue <= inactivePowerSetting && deviceActive){
+				sendEvent(name:"acceleration", value:"inactive", displayed:false)
+			}
+			refreshHistory
+		} else {
+			sendEvent(event)
+		}
+	} else if (description?.startsWith("read attr -")) {
+		def descMap = zigbee.parseDescriptionAsMap(description)
+		// log.debug "Desc Map: $descMap"
+		if (descMap.clusterInt == zigbee.ELECTRICAL_MEASUREMENT_CLUSTER) {
+			def intVal = Integer.parseInt(descMap.value,16)
+			if (descMap.attrInt == 0x0600) {
+				// log.debug "ACVoltageMultiplier $intVal"
+				state.voltageMultiplier = intVal
+			} else if (descMap.attrInt == 0x0601) {
+				// log.debug "ACVoltageDivisor $intVal"
+				state.voltageDivisor = intVal
+			} else if (descMap.attrInt == 0x0602) {
+				// log.debug "ACCurrentMultiplier $intVal"
+				state.currentMultiplier = intVal
+			} else if (descMap.attrInt == 0x0603) {
+				// log.debug "ACCurrentDivisor $intVal"
+				state.currentDivisor = intVal
+			} else if (descMap.attrInt == 0x0604) {
+				// log.debug "ACPowerMultiplier $intVal"
+				state.powerMultiplier = intVal
+			} else if (descMap.attrInt == 0x0605) {
+				// log.debug "ACPowerDivisor $intVal"
+				state.powerDivisor = intVal
+			} else if (descMap.attrInt == 0x0505) {
+				def voltageValue = intVal * getVoltageMultiplier()
+				// log.debug "Voltage ${voltageValue}"
+				state.voltage = $voltageValue
+				sendEvent(name: "voltage", value: voltageValue)
+			} else if (descMap.attrInt == 0x0508) {
+				def currentValue = intVal * getCurrentMultiplier()
+				// log.debug "Current ${currentValue}"
+				state.current = $currentValue
+				sendEvent(name: "current", value: currentValue)
+			}
+		} else {
+			log.warn "Not an electrical measurement"
+		}
+	} else {
+		log.warn "DID NOT PARSE MESSAGE for description : $description"
+		log.debug zigbee.parseDescriptionAsMap(description)
+	}
 }
